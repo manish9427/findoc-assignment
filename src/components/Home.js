bfa9e81c2dfd29e9d5f2ec5d0ec7ef34c0c3da9c
manish9427 <manish119427@gmail.com>
2025-07-24 17:37:28 +0530

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
     const fetchImages = async () => {
       try {
         const response = await axios.get('https://picsum.photos/v2/list?page=1&limit=10');
+        console.log('Fetched images:', response.data);
         setImages(response.data);
       } catch (error) {
         console.error('Error fetching images:', error);
@@ -25,9 +26,6 @@ const Home = () => {
           <div key={image.id} style={{ marginBottom: '10px' }}>
             <img src={image.download_url} alt={image.author} style={{ width: '100%', height: 'auto' }} />
             <h3 style={{ fontFamily: 'Montserrat, sans-serif', fontWeight: '600', color: 'black' }}>{image.author}</h3>
-            <p style={{ fontFamily: 'Montserrat, sans-serif', color: 'darkgrey', overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}>
-              {image.description || 'No description available'}
-            </p>
           </div>
         ))}
       </div>
